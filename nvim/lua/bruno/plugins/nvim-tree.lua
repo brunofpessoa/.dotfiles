d01529d60f1bae565616d134cfba2066f6c2b3ba
Bruno Pessoa <71235114+brunofpessoa@users.noreply.github.com>
2023-12-24 20:57:11 -0300

    booomn

--- nvim/lua/bruno/plugins/nvim-tree.lua
+++ nvim/lua/bruno/plugins/nvim-tree.lua
@@ -4,7 +4,6 @@ return {
 	event = "VimEnter",
 	config = function()
 		local nvimtree = require("nvim-tree")
-
 		-- recommended settings from nvim-tree documentation
 		vim.g.loaded_netrw = 1
 		vim.g.loaded_netrwPlugin = 1
@@ -14,6 +13,12 @@ return {
 
 		-- configure nvim-tree
 		nvimtree.setup({
+			sync_root_with_cwd = true,
+			respect_buf_cwd = true,
+			update_focused_file = {
+				enable = true,
+				update_root = true,
+			},
 			view = {
 				width = tree_width,
 				relativenumber = true,
--- nvim/lua/bruno/plugins/project-manager.lua
+++ nvim/lua/bruno/plugins/project-manager.lua
@@ -4,6 +4,13 @@ return {
 		-- enable saving the state of plugins in the session
 		vim.opt.sessionoptions:append("globals") -- save global variables that start with an uppercase letter and contain at least one lowercase letter.
 	end,
+	opts = {
+		projects = {
+			"~/workspace/*",
+			"~/.dotfiles",
+		},
+		last_session_on_startup = false,
+	},
 	dependencies = {
 		{ "nvim-lua/plenary.nvim" },
 		{ "nvim-telescope/telescope.nvim", tag = "0.1.4" },
@@ -11,21 +18,5 @@ return {
 	},
 	lazy = false,
 	priority = 100,
-	config = function()
-		require("neovim-project").setup({
-			projects = {
-				"~/workspace/*",
-				"~/.dotfiles",
-			},
-			last_session_on_startup = false,
-			require("nvim-tree").setup({
-				sync_root_with_cwd = true,
-				respect_buf_cwd = true,
-				update_focused_file = {
-					enable = true,
-					update_root = true,
-				},
-			}),
-		})
-	end,
+	config = true,
 }
